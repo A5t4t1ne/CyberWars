dcf0815c2c548b6cf133de940619e07b73ef6140
A5t4t1ne <A5t4t1ne@users.noreply.github.com>
2024-10-11 08:12:36 +0200

    add index and review page, XSS vulnerability, bootstrap, looks like shit

--- docker_env/web-server/src/app.py
+++ docker_env/web-server/src/app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+# from flask import Markup
 
 app = Flask(__name__)
+reviews: list[str] = [  "Best park ever!!!! Had the best time of my life. Everything was perfect, food, rides, people, all of it. 10/10", 
+                        "Ok for it's money I guess", 
+                        "The cardboard ride of my 5 year old son gives you more adrenaline, than whatever you put there.", 
+                        "Meh", 
+                        "You suck. -3/5"]
 
 @app.route("/")
-def hello():
+def index():
    return render_template('index.html')
+
+
+@app.route("/review", methods = ['GET', 'POST'])
+def review():
+    if request.method == 'POST':
+        new_review = request.form.get("review-txt", None)
+        if new_review:
+            reviews.append(new_review)
+    return render_template('review.html', reviews=reviews)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
